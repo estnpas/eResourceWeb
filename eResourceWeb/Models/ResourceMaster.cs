c0d6d038175dbd6c92b6d95db2ebbabec53a867f
estnpas <steven.parks@ericsson.com>
2014-01-07 15:02:20 -0800

    Class to handle the ResourceMaster

---
+++ eResourceWeb/Models/ResourceMaster.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace eResourceWeb.Models
+{
+
+    /// <summary>
+    /// Summary description for ResourceMaster
+    /// </summary>
+    public class ResourceMaster
+    {
+        
+        public float ResourceId { get; set; }
+        public string ResourceName { get; set; }
+       
+    }
+}
