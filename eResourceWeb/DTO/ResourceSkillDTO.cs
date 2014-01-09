7d691e08b8cd557f45782d1a5199f22fd0de6add
estnpas <steven.parks@ericsson.com>
2014-01-09 15:11:24 -0800

    New models to handle the resource-assigned skills inventory

---
+++ eResourceWeb/DTO/ResourceSkillDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace eResourceWeb.DTO
+{
+    public class ResourceSkillDTO : BaseDTO
+    {
+        private int ResourceId { get; set; }
+        private int SkillGroupId { get; set; }
+        private string SkillGroupName { get; set; }
+        private int SkillId { get; set; }
+        private string SkillName { get; set; }
+    }
+}
